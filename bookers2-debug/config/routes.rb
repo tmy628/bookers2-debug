50a36140b64a29260596bb32810bd9b9970ba8a3
tmy628 <tmy6h1r@gmail.com>
2021-05-01 12:46:45 +0000

    [fix] サーバー起動時、起動後　サインアップをする前、サインアップ時の不具合を修正

--- bookers2-debug/config/routes.rb
+++ bookers2-debug/config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  resources :users,only: [:show,:index,:edit,:update]
-  resources :books
   devise_for :users
+
   root 'homes#top'
+
+  resources :users,only: [:show,:index,:edit,:update]
+
+  resources :books
+
   get 'home/about' => 'homes#about'
+end
